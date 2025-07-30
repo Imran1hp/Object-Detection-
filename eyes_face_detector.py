f0a51d3246845391cc5ff4e0ec92daed8b78bae3
ali imran <aliimranlaskar123@gmail.com>
2025-07-30 22:39:46 +0530

    finish project

--- eyes_face_detector.py
+++ eyes_face_detector.py
@@ -1,19 +1,29 @@
-import cv2 as cv
+import cv2
 import numpy as np
 
-cap = cv.VideoCapture(0)
-face_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
-eye_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_eye.xml')
+cap = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 while True:
     ret,frame = cap.read()
 
 
-    gary = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+    gary = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gary,1.3,5)
-    cv.imshow('frame',frame)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),5)
+        roi_gray = gary[y:y+h,x:x+w]
+        roi_color = frame[y:y+h,x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray,1.3,5)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),5)
 
-    if cv.waitKey(1) & 0xFF == ord('q'):
+    cv2.imshow('frame',frame)
+
+        
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
-cv.destroyAllWindows()   
+cv2.destroyAllWindows()   
